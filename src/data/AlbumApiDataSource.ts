f55d988bbba32db4257afd1068904510ef36a64a
Román APF <romanapf17@gmail.com>
2023-08-04 12:50:02 +0200

    refactor: Add data caching in fetch operations

--- src/data/AlbumApiDataSource.ts
+++ src/data/AlbumApiDataSource.ts
@@ -2,15 +2,25 @@ import { Album } from '@/domain/album'
 import { Photo } from '@/domain/photo'
 
 export async function getAllAlbumsFromApi(): Promise<Album[]> {
-  const response = await fetch('https://jsonplaceholder.typicode.com/albums')
+  const response = await fetch('https://jsonplaceholder.typicode.com/albums', {
+    next: {
+      revalidate: 86400,
+    },
+  })
   const albums = await response.json()
   return albums
 }
 
 export async function getAlbumByIdFromApi(id: number): Promise<Album | null> {
   const response = await fetch(
-    `https://jsonplaceholder.typicode.com/albums/${id}`
+    `https://jsonplaceholder.typicode.com/albums/${id}`,
+    {
+      next: {
+        revalidate: 86400,
+      },
+    }
   )
+
   if (response.ok) {
     const album = await response.json()
     return album
@@ -23,7 +33,12 @@ export async function getPhotosByAlbumIdFromApi(
   albumId: number
 ): Promise<Photo[]> {
   const response = await fetch(
-    `https://jsonplaceholder.typicode.com/photos?albumId=${albumId}`
+    `https://jsonplaceholder.typicode.com/photos?albumId=${albumId}`,
+    {
+      next: {
+        revalidate: 86400,
+      },
+    }
   )
   const photos = await response.json()
   return photos
